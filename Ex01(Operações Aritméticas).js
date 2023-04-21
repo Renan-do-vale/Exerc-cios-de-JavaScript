51127306a18c87dc198c95b8eefbd8080b1442ba
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-04-21 20:27:11 -0300

    Ex01(operações Aritméticas)

---
+++ Ex01(Operações Aritméticas).js
@@ -0,0 +1,9 @@
+function CalcularOperacoes (n1, n2) {
+    console.log(`${n1} + ${n2} = ${n1 + n2} `)
+    console.log(`${n1} - ${n2} = ${n1 - n2} `)
+    console.log(`${n1} x ${n2} = ${n1 * n2} `)
+    console.log(`${n1} / ${n2} = ${n1 / n2} `)
+}
+
+
+CalcularOperacoes(10, 5)
