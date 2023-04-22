ef63d5029f4b389a5f74268c3571c961bdbea472
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-04-21 21:32:46 -0300

    Ex03

---
+++ Ex03(base Elevada).js
@@ -0,0 +1,10 @@
+/*
+03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+*/
+
+function ElevaExpoente (base, expoente) {
+    return base ** expoente
+}
+
+
+console.log(ElevaExpoente(5, 4))
