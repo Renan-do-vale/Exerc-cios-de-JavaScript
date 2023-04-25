b057b01cd8ae55dccb523850dce5be094c8bbcc1
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-04-25 19:49:11 -0300

    Ex04

---
+++ Ex04(resto da divisão).js
@@ -0,0 +1,11 @@
+/*
+04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.
+*/
+
+RestoDivisao = (a, db) => {
+    console.log(`${a} / ${b} = ${a / b} e o resto da divisão é ${a % b}`)
+}
+
+
+RestoDivisao(7, 2)
