37047828039292bdc604d3e54d1b456732cc1b24
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-05-09 21:06:50 -0300

    Ex12v1

--- Ex12(Dia util).js
+++ Ex12(Dia util).js
@@ -10,27 +10,34 @@ function DiaUtil(dia) {
     switch(dia){
         case 0:
             console.log('Sunday')
+            console.log('Weekend')
             break
         case 1:
             console.log('Monday')
+            console.log('Business day')
             break
         case 2:
             console.log('Tuesday')
+            console.log('Business day')
             break
         case 3:
             console.log('Wednesday')
+            console.log('Business day')
             break
         case 4:
             console.log('Thursday')
+            console.log('Business day')
             break
         case 5:
             console.log('Friday')
+            console.log('Business day')
             break
         case 6:
             console.log('Saturday')
+            console.log('Weekend')
             break
         default:
-            console.log('Erro')
+            console.log('Invalid day')
     }
 
 }
