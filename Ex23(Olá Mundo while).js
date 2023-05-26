354daccd68323ab3c320055c7166c51b42c462f2
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-05-26 12:16:55 -0300

    Create Ex23(Olá Mundo while).js

---
+++ Ex23(Olá Mundo while).js
@@ -0,0 +1,13 @@
+/*
+Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+*/
+
+function mundoRepetido() {
+    cont = 0
+    while(cont < 11) {
+        console.log("Hello World!")
+        cont++
+    }
+}
+
+mundoRepetido()
